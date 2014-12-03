1efbcc9620fb803ee85f8e5d435067f245e097c9
Ruslan <stelss1986@gmail.com>
2014-12-03 15:57:39 +0200

    Обработка смс с координатами, вывод результата

--- www/js/index.js
+++ www/js/index.js
@@ -18,6 +18,8 @@
  */
 //var smsplugin = cordova.require("info.asankan.phonegap.smsplugin.smsplugin");
 var smsplugin = null;
+var myLocation = {};
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -84,7 +86,7 @@ $('#sendSMS').on( "click", function(){
 $('#getSMS').on( "click", function(){
     alert('start get sms');
     smsplugin.isSupported(function(result){alert('isSuported => ');}, function(error){alert('Suported Error ');});
-    smsplugin.startReception(function(result){alert('sms=>' + result);}, function(error){alert('error '+ error);});
+    smsplugin.startReception(getSMSResult, function(error){alert('error '+ error);});
     alert('end get sms');
 });
 
@@ -97,11 +99,19 @@ $('#stopGetSMS').on( "click", function(){
 
 var markers = {};
 
-navigator.geolocation.getCurrentPosition (geolocationSuccess);
+navigator.geolocation.getCurrentPosition (
+                                          geolocationSuccess,
+                                          function(){alert('Error Get My Location')},
+                                          { maximumAge: 50000, timeout: 5000 }
+                                         );
 
 var map = L.map('map');
 
 function geolocationSuccess(position) {
+	myLocation['lat'] = position.coords.latitude;
+	myLocation['lng'] = position.coords.longitude;
+	myLocation['accuracy'] = position.coords.accuracy;
+
     alert (' Широта: ' + position.coords.latitude + "\n" + ' Долгота: ' + position.coords.longitude + "\n" + ' Высота: ' + position.coords.altitude + "\n" + ' точность: ' + position.coords.accuracy + "\n" + ' высоте точность: ' + position.coords.altitudeAccuracy + "\n" + ' заголовок: ' + position.coords.heading + "\n" + ' скорость: ' + position.coords.speed + "\n" + ' штампа времени: ' + position.timestamp + "\n");
 
     map.setView([position.coords.latitude, position.coords.longitude], 13);
@@ -112,33 +122,95 @@ function geolocationSuccess(position) {
       }).addTo(map);
 
     //set marker
-    var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+    var marker = L.marker([myLocation['lat'], myLocation['lng']]).addTo(map);
 
     //set  sircle radius
     //position.coords.accuracy - точность
-    var circle = L.circle([position.coords.latitude, position.coords.longitude], position.coords.accuracy, {
+    var circle = L.circle([myLocation['lat'], myLocation['lng']], position.coords.accuracy, {
         color: 'green',
         fillColor: '#00ff00',
         fillOpacity: 0.3
     }).addTo(map);
 
-  
+
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+    var curentTime = hours+':'+minutes+':'+seconds
+   
+
     //Сообщение для маркера
-    marker.bindPopup("<b>Вы</b><br>Ваше место нахождения.").openPopup();
-    setObjectPosition(49.4370, 32.0258, 'Rus');
+    marker.bindPopup("<b>Вы</b><br>Ваше место нахождения.<br> time: "+curentTime).openPopup();
+	
+	//Место нахождения другого обекта
+	var objLocation = {};
+	objLocation['lat'] = 49.4370;
+	objLocation['lng'] = 32.0258;
+	
+  setObjectPosition(objLocation['lat'], objLocation['lng'], 'Rus');
 
 };
 
+function getSMSResult(result) {
+  alert(result);
+  var smsTextMapData = getUrlParams( result, 'q' );
+  alert(smsTextMapData);
+  
+  var LatLng = splitLatLng(smsTextMapData);
+  setObjectPosition(LatLng[0], LatLng[1], 'smsResult');
+}
 
+////////////////////////////////////////////////////
+////////////// Измерение дистанции /////////////////
+////////////////////////////////////////////////////
+function distanceKM(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d.toFixed(2);
+}
 
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+
+function getUrlParams( url, name )
+{
+  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+  var regexS = "[\\?&]"+name+"=([^&#]*)";
+  var regex = new RegExp( regexS );
+  var results = regex.exec( url );
+  if( results == null )
+    return null;
+  else
+    return results[1];
+}
+
+function splitLatLng(str) {
+  return str.split(",");
+}
+
+///////////// Отображение или пермещение маркера /////////////////////////
 function setObjectPosition(lat, lng, name) 
 {
-    alert(markers[name]);
-    if(markers[name] === undefined) {
+    alert('obj start');
+    var distance = distanceKM(myLocation['lat'], myLocation['lng'], lat, lng);
+    alert(distance);
+    if(markers[name] === undefined || markers[name] === null) {
         alert('new marker');
         markers[name] = L.marker([lat, lng]).addTo(map);
-        markers[name].bindPopup("<b>Object1 ["+name+"]</b><br>Место нахождения.").openPopup();
+        markers[name].bindPopup("<b>Object1 ["+name+"]</b><br>Растояние:"+distance+"KM").openPopup();
     } else {
         markers[name].setLatLng([lat, lng]).update();
     }
+    alert('obj end');
 }
+
