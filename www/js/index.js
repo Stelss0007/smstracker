bfea5676100029a9a3edccf3819a98667d58ee78
Ruslan <stelss1986@gmail.com>
2014-12-04 17:29:34 +0200

    use local map

--- www/js/index.js
+++ www/js/index.js
@@ -116,9 +116,10 @@ function geolocationSuccess(position) {
 
     map.setView([position.coords.latitude, position.coords.longitude], 13);
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+//    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    L.tileLayer('file:///sdcard/tiles/{z}/{x}/{y}.jpg', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-      maxZoom: 18
+      maxZoom: 13
       }).addTo(map);
 
     //set marker
